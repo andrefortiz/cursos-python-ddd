a072e2c2008b49d1735663be56e76b8fed69b71a
andrefortiz <andre.fortiz@gmail.com>
2023-04-11 20:01:34 -0300

    Refatoração e ajustes na estrutura

--- code/src/manutencao/solicitacao/aplicacao/SolicitacoesDeManutencao/AbstractBuscadorDeContrato.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-from manutencao.solicitacao.aplicacao.SolicitacoesDeManutencao.ContratoDto import ContratoDto
-
-
-class AbstractBuscadorDeContrato(abc.ABC):
-    @abc.abstractmethod
-    def buscar(self, numero: str) -> ContratoDto:
-        pass
--- code/src/manutencao/solicitacao/dominio/SolicitacoesDeManutencao/AbstractCanceladorDeSolicitacoesDeManutencaoPendentes.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-from manutencao.solicitacao.dominio.SolicitacoesDeManutencao.SolicitacaoDeManutencao import SolicitacaoDeManutencao
-
-
-class AbstractCanceladorDeSolicitacoesDeManutencaoPendentes(abc.ABC):
-    @abc.abstractmethod
-    def cancelar(self, solicitacoes_de_manutecao_pendentes: [SolicitacaoDeManutencao]):
-        pass
